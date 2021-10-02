2009b04fe17b918b325828c82dbb50b929b967d9
Imran Azam <imranazam3@gmail.com>
2021-10-03 00:03:20 +0100

    added animations

--- js/main.js
+++ js/main.js
@@ -25,9 +25,25 @@ const displayRandomJoke = () => {
   txt.innerHTML = selftext;
   document.getElementById("selftext").innerHTML = "<p>" + txt.value + "</p>";
 
+  animateCSS("joke", "tada");
   document.getElementById("joke").classList.remove("hidden");
 };
 
-//getJoke();
+const animateCSS = (element, animation, prefix = "animate__") => {
+  // We create a Promise and return it
+  return new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.getElementById(element);
 
-const hello = (arg) => console.log("ciao", arg);
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
+};
